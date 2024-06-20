ea4972078f63bbc1c2ee8e9c2c3a135e709dced6
krishna pradhan <kpaccess@gmail.com>
2024-06-20 09:33:24 +0530

    added handle click

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { increment } from "./features/counter/counter-slice";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => dispatch(increment());
 
   return (
     <>
@@ -18,9 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleClick}>count is : {count} </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
